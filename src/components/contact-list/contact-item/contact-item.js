369fbdf1db1a59fc49c85aea6a78d9bf8adf5ffa
trofimchuk <trofimchuk@itstep.academy>
2023-01-09 17:35:04 +0200

    Added empty project

---
+++ src/components/contact-list/contact-item/contact-item.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContactItem = () => {
+  return <h1>Contact Item</h1>;
+};
+
+export default ContactItem;
---
+++ src/components/contact-list/contact-list.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ContactItem from "./contact-item/contact-item";
+
+const ContactList = () => {
+  return (
+    <h1>
+      <ContactItem />
+      <ContactItem />
+      <ContactItem />
+      <ContactItem />
+    </h1>
+  );
+};
+
+export default ContactList;
