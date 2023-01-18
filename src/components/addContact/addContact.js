5146977a1d6a86e4fc45086894ab2ddd9503d4b0
trofimchuk <trofimchuk@itstep.academy>
2023-01-18 15:38:30 +0200

    add new contact

--- src/components/addContact/addContact.js
+++ src/components/addContact/addContact.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddContact = () => {
-  return <h1>New contact</h1>;
+  const [name, setName] = useState("John Doe");
+  const [phone, setPhone] = useState("+xx(xxx)xx-xx-xxx");
+
+  const onGetName = (event) => {
+    setName(event.target.value);
+  };
+
+  const onGetPhone = (event) => {
+    setPhone(event.target.value);
+  };
+
+  return (
+    <div className="row">
+      <div className="col-7">
+        <h2>Add new contact</h2>
+        <form>
+          <div className="mb-3">
+            <div className="form-label">Name</div>
+            <input
+              type="text"
+              className="form-control"
+              value={name}
+              onChange={onGetName}
+            />
+          </div>
+          <div className="mb-3">
+            <div className="form-label">Phone</div>
+            <input
+              type="text"
+              className="form-control"
+              value={phone}
+              onChange={onGetPhone}
+            />
+          </div>
+        </form>
+      </div>
+      <div className="col-5">User info</div>
+    </div>
+  );
 };
 
 export default AddContact;
